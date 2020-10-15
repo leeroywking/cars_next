006178b911dfad51ace93230d33956680487c9ea
Lee-Roy King <leeroywking@gmail.com>
2020-10-14 21:25:13 -0700

    hacked it yo

--- pages/_Carslist.js
+++ pages/_Carslist.js
@@ -1,5 +1,9 @@
 const List = (props) => {
+    if(!props.list){
+        props.list = [{make:"nothing", model: "nothing", price:0}]
+    }
     return <div>
+        
         {props.list.length} cars in garage
         {props.list.map(car => <li>
             {car.make} {car.model} ${car.price}
